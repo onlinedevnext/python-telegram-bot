e03b0a229485ec3535a1503951b6df66766c5530
onlinedevnext <onlineearn.gt@gmail.com>
2025-04-20 13:41:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv
 
+# Load from .env in dev / local
 load_dotenv()
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-MONGODB_URI = os.getenv("MONGODB_URI")
+MONGO_URI = os.getenv("MONGODB_URI")
 BASE_URL = os.getenv("BASE_URL")
